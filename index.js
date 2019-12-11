805bfa73b877ee10212169b379fea200253ede5e
Ross Chiu <18388411+screw123@users.noreply.github.com>
2019-12-11 17:03:06 +0800

    update to remove support of redux-dynamic-module

---
+++ index.js
@@ -0,0 +1,56 @@
+const windowSizeReducer = (
+	state = {
+		width: typeof window === "object" ? window.innerWidth || 100 : 100,
+		height: typeof window === "object" ? window.innerWidth || 100 : 100,
+		fontSize:
+			typeof document === "object"
+				? parseFloat(getComputedStyle(document.body).fontSize) || 16
+				: 16,
+		position:
+			typeof document === "object"
+				? document.body.scrollTop || document.documentElement.scrollTop || 0
+				: 0
+	},
+	{ type, data }
+) => {
+	switch (type) {
+		case "windowSize.UpdateSize":
+			return Object.assign({}, state, {
+				width: data.width,
+				height: data.height
+			})
+		case "windowSize.UpdatePosition":
+			return Object.assign({}, state, { position: data.position })
+		case "windowSize.UpdateFontSize":
+			return Object.assign({}, state, { fontSize: data.fontSize })
+		default:
+			console.log("default called")
+			return state
+	}
+}
+/**
+ * testing
+ */
+exports.getSizeAction = () => {
+	return {
+		type: "windowSize.UpdateSize",
+		data: {
+			width: typeof window === "object" ? window.innerWidth : 100,
+			height: typeof window === "object" ? window.innerHeight : 100
+		}
+	}
+}
+
+exports.getPositionAction = () => {
+	return {
+		type: "windowSize.UpdatePosition",
+		data: {
+			position:
+				typeof document === "object"
+					? document.body.scrollTop || document.documentElement.scrollTop || 0
+					: 0
+		}
+	}
+}
+
+exports.default = windowSizeReducer
---
+++ readme.md
@@ -0,0 +1,14 @@
+use with `redux-dynamic-modules`
+
+give:
+
+```
+windowSize : {
+	width, height, position
+}
+```
+
+## to use
+
+1. `yarn add redux-dynamic-modules`
+2. `yarn add @screw123/redux-windowsize`
